205cec6851405807a53afecf27b90553e4ef4baa
Jordan Siaw <jordansiaw.cy@gmail.com>
2016-02-19 23:31:38 +0000

    Add helper function to properly title views. Add controller tests for static_pages.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @base_title = "Dine for Two"
+  end
+
+  test "should get home" do
+    get :home # A controller action.
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+    # Selects the title tag and asserts it matches the given string
+  end
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
 end
