7bf7d45ba5be122e8e52b8098919438cfa11bc70
cmalla94 <cmalla@sfu.ca>
2016-02-20 00:15:00 -0800

    Sessions setup, log in feature working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get "log_in" => 'sessions#new', :as => "log_in"
+  post 'log_in' => 'sessions#create', :as => "logged_in"
+  delete 'log_out' => 'sessions#destroy', :as => "log_out"
 
   get "sign_up" => "people#new", :as => "sign_up"
+
 
   root 'people#new'
 
