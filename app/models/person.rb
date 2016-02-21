aed69db69a888575bec2fddc39e8ef361ca5a6b3
dzrcarl <nuclearcarl@gmail.com>
2016-02-21 23:12:01 +0000

    change the validation parameters for user name, min -> 2

--- app/models/person.rb
+++ app/models/person.rb
@@ -6,7 +6,7 @@ class Person < ActiveRecord::Base
                     length: {minimum: 7}
   validates_uniqueness_of :email
   validates :name,  presence: true,
-                    length: {minimum: 5, maximum: 12}
+                    length: {minimum: 2, maximum: 12}
   validates_uniqueness_of :name
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}
