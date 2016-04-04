0f175b44a00f931fe02394bac9c20d8df60f7649
jinquanjiang <aiouquan@hotmail.com>
2016-04-04 23:52:43 +0000

    change the index page and show page for the restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,7 @@
 class RestaurantsController < ApplicationController
   def index
-    @restaurants = Restaurant.all
+    # @restaurants = Restaurant.all
+    @restaurants = Restaurant.paginate(page: params[:page])
   end
 
   def show
